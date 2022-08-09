d876d584ce4b56442fa27eb680a6a8e26c1e8b8a
Cristhian Intriago <104945951+cristhianim@users.noreply.github.com>
2022-08-08 20:56:17 -0500

    Add files via upload
    Creacion de repositorio y subida del primer prototipo

---
+++ script.js
@@ -0,0 +1,11 @@
+function iniciarSesion() {
+    let botonIniciarSesion = document.getElementById("btn-iniciarSesion");
+    botonIniciarSesion.addEventListener("click", input-container);
+  
+    let botonFuego = document.getElementById("boton-fuego");
+    botonFuego.addEventListener("click", ataqueFuego);
+    let botonAgua = document.getElementById("boton-agua");
+    botonAgua.addEventListener("click", ataqueAgua);
+    let botonTierra = document.getElementById("boton-tierra");
+    botonTierra.addEventListener("click", ataqueTierra);
+  }
