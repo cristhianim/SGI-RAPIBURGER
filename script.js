8b95a24b2d2ce6ef4ed0c9774e3dc96f60998fba
Cristhian Intriago <cristhianim@outlook.es>
2022-08-09 20:08:00 -0500

    Correciones

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function iniciarSesion() {
-    let botonIniciarSesion = document.getElementById("btn-iniciarSesion");
-    botonIniciarSesion.addEventListener("click", input-container);
-  
-    let botonFuego = document.getElementById("boton-fuego");
-    botonFuego.addEventListener("click", ataqueFuego);
-    let botonAgua = document.getElementById("boton-agua");
-    botonAgua.addEventListener("click", ataqueAgua);
-    let botonTierra = document.getElementById("boton-tierra");
-    botonTierra.addEventListener("click", ataqueTierra);
-  }
+function myScript(){
+  document.getElementById(btn-iniciarSesion).style.color(red)
+}
